7de316e388baa165fbab95c6bad1b13a94ae590c
Jamal Ashraf <jamalashraf0406@gmail.com>
2025-12-28 22:56:52 +0530

    initial commit

---
+++ src/test/java/com/yutube/registery_service/RegisteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yutube.registery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisteryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
